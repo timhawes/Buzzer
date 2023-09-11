280a5f2c3aded6e0f67502ee1ecb5c9b0fa76349
Tim Hawes <me@timhawes.com>
2023-09-11 22:18:21 +0100

    GitHub CI workflow

---
+++ examples/test/main.cpp
@@ -0,0 +1,17 @@
+#include "Buzzer.hpp"
+
+buzzer_note trill[] = { {1, 20}, {0, 20}, {1, 20}, {0, 20}, {1, 20}, {0, 0} };
+
+Buzzer buzzer(13, true);
+
+void setup() {
+    buzzer.click();
+    buzzer.chirp();
+    buzzer.beep(100);
+    buzzer.beep(100, 1000);
+    buzzer.play(trill);
+}
+
+void loop() {
+
+}
